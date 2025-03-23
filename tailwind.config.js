e68fa861b1c21c99251df800c55da4e18d623fe0
Hiwot Belay <hiwotbelay060@gmail.com>
2025-03-23 16:03:04 +0300

    Updated project in some codes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // Ensures Tailwind scans all your HTML and JS files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
